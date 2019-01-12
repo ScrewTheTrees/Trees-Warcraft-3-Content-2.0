49fbf0eb4c2c2276b168597672fc439854de9354
ScrewTheTrees <fredrik@grimmenhag.se>
2019-01-12 16:43:04 +0100

    -

---
+++ Jass/PersonalAI/launch3_2v2.bat
@@ -0,0 +1,3 @@
+XCOPY "D:\Dropbox\Attacka Dropbox\Warcraft 3 Content\Jass\PersonalAI\TestNE.ai" "C:\Users\Fredrik Grimmenhag\Documents\Warcraft III\Ai\PersonalAI\TestAi.ai" /Y
+XCOPY "D:\Dropbox\Attacka Dropbox\Warcraft 3 Content\Jass\PersonalAI\TestGame2v2.wgc" "C:\Users\Fredrik Grimmenhag\Documents\Warcraft III\Ai\PersonalAI\TestGameNe.wgc" /Y
+"D:\Games\Warcraft III\Warcraft III.exe" -loadfile "C:\Users\Fredrik Grimmenhag\Documents\Warcraft III\Ai\PersonalAI\TestGameNe.wgc" -nowfpause -windowed
---
+++ WC3Map/ShootOnSight/Map Jass/Init.j
@@ -0,0 +1,16 @@
+library Map initializer InitMap
+    globals
+        public force forcePlayers = CreateForce()
+        public force forceAllies = CreateForce()
+        public force forceEnemies = CreateForce()
+    endglobals
+
+
+
+    private function MakePlayerTeams takes nothing returns nothing
+        
+    endfunction
+    private function InitMap takes nothing returns nothing
+
+    endfunction
+endlibrary
